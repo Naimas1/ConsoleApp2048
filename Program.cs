72703eefb8bd91b5fc910a18808c58dfc41de909
Denis Riabchuk <Naimas@DESKTOP-68Q4MHG>
2023-11-12 12:11:43 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp2048
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
